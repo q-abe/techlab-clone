405d2692ed473edac9ea0ca8d6fdcb7c31be67f6
q-abe <abe@q-co.jp>
2024-01-30 15:26:10 +0900

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import React from "react";
+//書かなくて良くなるtsconfig
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
