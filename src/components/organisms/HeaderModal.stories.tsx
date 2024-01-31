543dc6383b605bce4c43bb938417eab63bf4ca3a
q-abe <abe@q-co.jp>
2024-01-31 16:59:56 +0900

    Strorybook表示のために各コンポーネントに簡単に記述

---
+++ src/components/organisms/HeaderModal.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { HeaderModal } from "./HeaderModal";
+
+const meta = {
+    title: "SearchBar",
+    component: HeaderModal,
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
+    argTypes: {
+    },
+} satisfies Meta<typeof HeaderModal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
