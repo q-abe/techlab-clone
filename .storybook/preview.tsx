80f79a6b2c1acce547b96652ada61cf2c3caf860
q-abe <abe@q-co.jp>
2024-02-01 12:07:52 +0900

    TODO：フォント対応のコメント追記

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,6 +4,7 @@ import { Global as EmotionGlobal, css } from "@emotion/react";
 import { withThemeFromJSXProvider } from "@storybook/addon-themes";
 
 const GlobalStyles = () => (
+    //TODO；デフォルトスタイル打ち消し対応。
   <EmotionGlobal
     styles={css`
         body {
