189b727f3ec4316315b315b8284fa7ebcfca988f
q-abe <abe@q-co.jp>
2024-02-01 16:28:18 +0900

    symbol内のArticlesのstory表示記述

--- src/components/atoms/symbols/ArticleThumbnail.stories.tsx
+++ src/components/atoms/symbols/ArticleThumbnail.stories.tsx
@@ -1,18 +1,23 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type {Meta, StoryObj} from "@storybook/react";
 
-import {　ArticleThumbnail　} from "./ArticleThumbnail";
+import {ArticleThumbnail} from "./ArticleThumbnail";
 
 const meta = {
-    title: "SearchBar",
+    title: "ArticleThumbnail",
     component: ArticleThumbnail,
     parameters: {
         layout: "centered",
     },
     tags: ["autodocs"],
-    argTypes: {
-    },
+    argTypes: {},
 } satisfies Meta<typeof ArticleThumbnail>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
+
+export const ThumbnailPicture: Story = {
+    args: {
+        label: "サムネイル画像"
+    },
+};
--- src/components/atoms/symbols/ArticleTitleText.stories.tsx
+++ src/components/atoms/symbols/ArticleTitleText.stories.tsx
@@ -1,6 +1,6 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type {Meta, StoryObj} from "@storybook/react";
 
-import {　ArticleTitleText　} from "./ArticleTitleText";
+import {ArticleTitleText} from "./ArticleTitleText";
 
 const meta = {
     title: "SearchBar",
@@ -9,10 +9,15 @@ const meta = {
         layout: "centered",
     },
     tags: ["autodocs"],
-    argTypes: {
-    },
+    argTypes: {},
 } satisfies Meta<typeof ArticleTitleText>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
+
+export const Text: Story = {
+    args: {
+        label: "記事タイトルのテキスト"
+    },
+};
