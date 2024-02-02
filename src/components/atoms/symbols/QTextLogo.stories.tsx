08001fb3614d8303653b9ccff77d36dce3ce3735
q-abe <abe@q-co.jp>
2024-02-02 13:35:54 +0900

    Qのテキストロゴを作成

--- src/components/atoms/symbols/QTextLogo.stories.tsx
+++ src/components/atoms/symbols/QTextLogo.stories.tsx
@@ -1,18 +1,25 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type {Meta, StoryObj} from "@storybook/react";
 
-import {　QTextLogo　} from "./QTextLogo";
+import {QTextLogo} from "./QTextLogo";
 
 const meta = {
-    title: "SearchBar",
+    title: "QTextLogo",
     component: QTextLogo,
     parameters: {
         layout: "centered",
     },
     tags: ["autodocs"],
     argTypes: {
+        backgroundColor: {control: 'color'}
     },
 } satisfies Meta<typeof QTextLogo>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
+
+export const ThumbnailPicture: Story = {
+    args: {
+        backgroundColor: "white"
+    },
+};
