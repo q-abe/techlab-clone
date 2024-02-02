aef84f3ecf6778d2e8e00c63c3e2f056832b77af
q-abe <abe@q-co.jp>
2024-02-02 22:19:55 +0900

    Qの赤いロゴを作成

--- src/components/atoms/symbols/QImgLogo.stories.tsx
+++ src/components/atoms/symbols/QImgLogo.stories.tsx
@@ -1,18 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import {　QImgLogo　} from "./QImgLogo";
+import { QImgLogo } from "./QImgLogo";
 
 const meta = {
-    title: "SearchBar",
+    title: "QImgLogo",
     component: QImgLogo,
     parameters: {
         layout: "centered",
     },
     tags: ["autodocs"],
-    argTypes: {
-    },
+    argTypes: {},
 } satisfies Meta<typeof QImgLogo>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
+
+export const QLogo: Story = {
+    args: {
+        backgroundColor: "white"
+    },
+}
