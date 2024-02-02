afb7c1c2bdb04420e8ab11f348616a0ef9ac018a
q-abe <abe@q-co.jp>
2024-02-02 23:02:40 +0900

    タグアイコン作成

--- src/components/atoms/symbols/TagIcon.stories.tsx
+++ src/components/atoms/symbols/TagIcon.stories.tsx
@@ -1,18 +1,19 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import {　TagIcon　} from "./TagIcon";
+import { TagIcon } from "./TagIcon";
 
 const meta = {
-    title: "SearchBar",
+    title: "TagIcon",
     component: TagIcon,
     parameters: {
         layout: "centered",
     },
     tags: ["autodocs"],
-    argTypes: {
-    },
+    argTypes: {},
 } satisfies Meta<typeof TagIcon>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
+
+export const Tag: Story = {};
--- src/components/atoms/symbols/TagIcon.tsx
+++ src/components/atoms/symbols/TagIcon.tsx
@@ -1,14 +1,24 @@
 import React, { FC } from "react";
 
-type TagIconProps = {
-};
+type TagIconProps = {};
 
 export const TagIcon: FC<TagIconProps> = (props) => {
-    const {
-    } = props;
+    const {} = props;
 
     return (
-        <div >
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="auto" viewBox="0 0 49.029 24" fill="#adadad">
+                <g id="グループ_251" data-name="グループ 251" transform="translate(-6.5 -14.668)">
+                    <g id="グループ_250" data-name="グループ 250" transform="translate(6.5 14.668)">
+                        <rect id="長方形_121" data-name="長方形 121" width="6.42" height="24"></rect>
+                        <path id="パス_543" data-name="パス 543"
+                              d="M51.978,26.668l-11.667,12v-24Zm-11.667-12H10.976v24H40.311Z"
+                              transform="translate(-2.95 -14.668)"></path>
+                    </g>
+                    <circle id="楕円形_14" data-name="楕円形 14" cx="3.21" cy="3.21" r="3.21"
+                            transform="translate(40.05 23.132)" fill="#fff"></circle>
+                </g>
+            </svg>
         </div>
     );
 };
