c9a6533469fbc9b81c3f4fbd2899481393cf2ec9
q-abe <abe@q-co.jp>
2024-02-16 23:14:53 +0900

    グローバルスタイル変更。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,21 @@ import type { AppProps } from "next/app";
 import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const global = css`
-    body {
-      color: white;
-      background-color: red;
-    }
+    const global = css`
+        body {
+            overflow: auto;
+            background-color: rgb(255, 255, 255);
+            color: rgb(102, 102, 102);
+            font-family: 游ゴシック, "Yu Gothic", 游ゴシック体, YuGothic, sans-serif;
+            font-size: 13px;
+            min-height: 100vh;
+            text-rendering: optimizeSpeed;
+            line-height: 1.5;
+            }
+
     ,
     button {
-      color: red;
-      background-color: blue;
-    }
-  `;
-  return <><Global styles={global} /><Component {...pageProps} /></>;
+        }
+    `;
+    return <><Global styles={global}/><Component {...pageProps} /></>;
 }
